73eb9083fe18f1d5cd63464125250c3fb316c58b
Jacqui <jt.cope1@gmail.com>
2022-06-03 16:16:11 +1000

    sets up count state

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -11,19 +11,24 @@ export const Counter = (props) => {
   // array destructuring to set up count state
   const { capacity } = props;
   const [count, setCount] = useState(0);
+  const [warning, setWarning] = useState("");
+
   const increment = () => {
     setCount(count + 1);
   };
 
+  //   first argument is a callback
+  //   second argument is an array of dependencies, if they change the callback will run again
   useEffect(() => {
+    console.log("I'm inside the useEffect");
     document.title = `Capacity: ${capacity}`;
-  });
+  }, [capacity]);
 
   //   return is purely jsx
   return (
     <>
       <CenteredBox direction="column">
-        <Warning centered>Warning</Warning>
+        {warning && <Warning centered>{warning}</Warning>}
         <Heading centered>{count}</Heading>
         <Row>
           <StyledButton onClick={() => setCount(count - 1)} variant="danger">
